7eb47683a80d56b48d07f3b1375bf186fe305cbc
Sergey Romanov <sergeytomanovv94@gmail.com>
2022-06-27 16:18:18 +0300

    board and init figures

---
+++ client/src/models/figures/Figure.ts
@@ -0,0 +1,39 @@
+import { Colors } from "../Colors";
+import logo from '../../assets/black-king.png'
+import { Cell } from "../Cell";
+import {v4} from 'uuid'
+
+export enum FigureNames {
+FIGURE = 'Фигура',
+KING = 'Король',
+QUEEN = 'Ферзь',
+ROOK = 'Ладья',
+BISHOP = 'Слон',
+KNIGHT = 'Конь',
+PAWN = 'Пешка',
+}
+
+export class Figure {
+  color: Colors;
+  logo: typeof logo | null;
+  cell: Cell;
+  name: FigureNames;
+  id: string;
+
+  constructor(color: Colors, cell: Cell) {
+    this.color = color;
+    this.cell = cell;
+    this.cell.figure = this;
+    this.logo = null;
+    this.name = FigureNames.FIGURE
+    this.id = v4()
+  }
+
+  canMove(target: Cell): boolean {
+    return true
+  }
+
+  moveFigure(target: Cell) {
+    
+  }
+}
