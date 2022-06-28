2128a17e236495a1070b7f49b219185050f00b81
Sergey Romanov <sergeytomanovv94@gmail.com>
2022-06-28 13:56:05 +0300

    Behavior of figures

--- client/src/models/figures/King.ts
+++ client/src/models/figures/King.ts
@@ -5,9 +5,28 @@ import blackLogo from '../../assets/black-king.png'
 import whiteLogo from '../../assets/white-king.png'
 
 export class King extends Figure {
-  constructor(color: Colors, cell: Cell){
+  constructor(color: Colors, cell: Cell) {
     super(color, cell);
     this.logo = color === Colors.BLACK ? blackLogo : whiteLogo;
     this.name = FigureNames.KING
+  }
+
+  canMove(target: Cell): boolean {
+    if (!super.canMove(target)) {
+      return false;
+    }
+    const direction = this.cell.figure?.color === Colors.BLACK ? 1 : -1;
+    
+    if ((target.y === this.cell.y + direction)
+      && target.x === this.cell.x && this.cell.board.getCell(target.x, target.y).isEmpty()) {
+      return true;
+    }
+    if (target.y === this.cell.y + direction
+      && (target.x === this.cell.x || target.x === this.cell.x)
+      && this.cell.isEnemy(target)) { // Проверяем перемещение по оси Х, проверяем на наличие врага
+      return true;
+    }
+
+    return false;
   }
 }
